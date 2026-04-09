523d44bc1543e27fa72e1a331a18f62401bd435c
ShaykhelislamovaDiana <Tyuiu.ShaykhelislamovaDA@outlook.com>
2026-04-09 11:16:25 +0500

    Task2

---
+++ Tyuiu.AngeliA.Sprint3.Task2.V3.Lib/Class1.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.AngeliA.Sprint3.Task2.V3.Lib
+{
+    public class Class1 : ISprint3Task2V3
+    {
+        public double GetSumSeries(int value, int startValue, int stopValue)
+        {
+            double sumSeries = 0;
+            do
+            {
+                sumSeries = sumSeries + (Math.Pow(value, 2) * startValue) + 1;
+                startValue++;
+            } while (startValue <= stopValue);
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.AngeliA.Sprint3.Task2.V3/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.AngeliA.Sprint3.Task2.V3.Lib;
+namespace Tyuiu.AngeliA.Sprint3.Task2.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
