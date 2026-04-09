a53f28a7694dafa5277f9f0c27bb7de16494d702
ShaykhelislamovaDiana <Tyuiu.ShaykhelislamovaDA@outlook.com>
2026-04-09 11:32:01 +0500

    Task4

---
+++ Tyuiu.AngeliA.Sprint3.Task4.V18.Lib/Class1.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.AngeliA.Sprint3.Task4.V18.Lib
+{
+    public class Class1 : ISprint3Task4V18
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double res = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    res = res + (Math.Sin(x) / Math.Cos(x));
+                }
+
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.AngeliA.Sprint3.Task4.V18.Test/Test1.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.AngeliA.Sprint3.Task4.V18.Lib;
+namespace Tyuiu.AngeliA.Sprint3.Task4.V18.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
