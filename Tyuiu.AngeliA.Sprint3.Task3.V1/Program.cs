a743de9805760b8b823e11757b0eb7faba390cd8
ShaykhelislamovaDiana <Tyuiu.ShaykhelislamovaDA@outlook.com>
2026-04-09 11:26:02 +0500

    Task3

---
+++ Tyuiu.AngeliA.Sprint3.Task3.V1/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.AngeliA.Sprint3.Task3.V1.Lib;
+namespace Tyuiu.AngeliA.Sprint3.Task3.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
