61c4e59ced17e5cb8215da1b2fb527bdf6e7d0d8
ShaykhelislamovaDiana <Tyuiu.ShaykhelislamovaDA@outlook.com>
2026-04-09 11:38:13 +0500

    Task5

---
+++ Tyuiu.AngeliA.Sprint3.Task5.V28.Lib/Class1.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.AngeliA.Sprint3.Task5.V28.Lib
+{
+    public class Class1 : ISprint3Task5V28
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sumseries = 0;
+            for (int i = startValue1; i <= stopValue1; i++)
+            {
+                for (int j = startValue2; j <= stopValue2; j++)
+                {
+                    sumseries += (Math.Pow(j, x) / Math.Sin(j));
+
+                }
+            }
+            return Math.Round(sumseries, 3);
+        }
+    }
+}
---
+++ Tyuiu.AngeliA.Sprint3.Task5.V28/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.AngeliA.Sprint3.Task5.V28.Lib;
+namespace Tyuiu.AngeliA.Sprint3.Task5.V28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
